daeea6253a1d374873562121ef6d409a719388c2
Neil Williams <Neil.Williams227@gmail.com>
2018-11-12 21:17:44 -0500

    added some more notes from React

--- lessons/React.js
+++ lessons/React.js
@@ -51,3 +51,41 @@ class MyComponent extends React.Component {
 };
 
 ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'))
+
+// Pass Props to a Stateless Functional Component ****
+
+const CurrentDate = (props) => {
+    return (
+        <div>
+            { /* change code below this line */}
+            <p>The current date is: {props.date} </p>
+            {/* passing the value of props */}
+            { /* change code above this line */}
+        </div>
+    );
+};
+
+
+class Calendar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h3>What date is it?</h3>
+                { /* change code below this line */}
+                <CurrentDate date={Date()} />
+                {/* instance of the date which is being used in the CurrentDate component */}
+                { /* change code above this line */}
+            </div>
+        );
+    }
+};
+
+// Pass an array in a prop ******
+// In the child element set the following
+return <p>{props.tasks.join(', ')}</p>
+
+    // In the parent element initialize the property with a value
+    < List tasks={["green", "blue", "red"]} />
